2ed236a220e8d11ead751897a4c8b60b4c04eb7d
Sami-Wafula <samiwafula3257@gmail.com>
2022-02-14 09:54:03 +0300

    Error fix

--- project_1/fibonacci_detector.py
+++ project_1/fibonacci_detector.py
@@ -1,35 +1,35 @@
-'''First assignment for the Lux Tech and Data Science EA Python and Java mentorship programme.
-A Fibonacci number detector.
-The program will:
-a)prompt for user input
-b)check the input value against a generated Fibonacci series of values
-c)output information wether the input value is Fibonacci or not
-Fibonacci logic:F(0) = 0
-                F(1) = 1
-                F(n) = F(n-1) + F(n-2)'''
+import sys
 
-
-def Fibonacci(x):
-     n1 = 0
-     n2 = 1
-     while n2<x:
-         n3 = n1+n2
-         n1 = n2
-         n2 = n3
-     if n2==x or n1==x:
-         return True
-     if n2 > x:
-         return False
+def fibonacci(num):
+    a = 0
+    b = 1
+    while b<num:
+        c = a+b
+        a = b
+        b = c
+    if b==num or a==num:
+        return True
+    if b > num:
+        return False
+    
 
 try:
-    x = int(input("Please enter the number you wish to check: "))
+    num = int(input("Please enter the number you wish to check: "))
 
 except ValueError:
     print("Please enter a valid number.")
-except NameError:
+
+try:
      
     
-     if Fibonacci(x):
-          print(f"{x} is a fibonacci number.")
-     else:
-          print(f"{x} is not a fibonacci number.")
+    if fibonacci(num):
+        print(f"{num} is a fibonacci number.")
+    else:
+        print(f"{num} is not a fibonacci number.")
+
+except NameError:
+    sys.exit()
+
+
+
+ 
